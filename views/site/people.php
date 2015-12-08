f080f1cb4a27c926338856be7cb8d113bff3860e
Tom <jiepengthegreat@126.com>
2015-12-08 16:17:05 +0800

    typo

--- views/site/people.php
+++ views/site/people.php
@@ -6,9 +6,9 @@
 	<?php if ($key & 1) :?>
 	<div style="width: 100%; height: 400px; margin-top: 20px;">
 		<img alt="" src="/<?=$member->pic?>"
-			style="width: 214px; float: left; margin: 10px;">
-		<div style="margin: 10px 10px 40px; float: left; width: 546px;">
-			<p style="text-align: right; font-size: 30px; line-height: 40px;"><?=$member->username?></p>
+			style="width: 214px; float: right; margin: 10px;">
+		<div style="margin: 10px 10px 40px; float: right; width: 546px;">
+			<p style="text-align: right; font-size: 30px; line-height: 40px;"><?=$member->name?></p>
 			<p
 				style="text-align: right; font-size: 20px; line-height: 30px; border-bottom: 1px solid #ccc;"><?=$member->position?></p>
 			<p style="text-align: right; font-size: 16px; line-height: 25px;"><?=$member->email?></p>
@@ -18,9 +18,9 @@
 	<?php else :?>
 	<div style="width: 100%; height: 400px;margin-top: 20px;">
 		<img alt="" src="/image/people-2.png"
-			style="width: 214px; float: right; margin: 10px;">
-		<div style="margin: 10px 10px 40px; float: right; width: 546px;">
-			<p style="text-align: left; font-size: 30px; line-height: 40px;"><?=$member->username?></p>
+			style="width: 214px; float: left; margin: 10px;">
+		<div style="margin: 10px 10px 40px; float: left; width: 546px;">
+			<p style="text-align: left; font-size: 30px; line-height: 40px;"><?=$member->name?></p>
 			<p
 				style="text-align: left; font-size: 20px; line-height: 30px; border-bottom: 1px solid #ccc;"><?=$member->position?></p>
 			<p style="text-align: left; font-size: 16px; line-height: 25px;"><?=$member->email?></p>
