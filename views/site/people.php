6773a2130d9a704dd6aa6243a37133cecbea8cd3
Tom <jiepengthegreat@126.com>
2015-12-08 16:30:06 +0800

    typo

--- views/site/people.php
+++ views/site/people.php
@@ -17,14 +17,14 @@
 	</div>
 	<?php else :?>
 	<div style="width: 100%; height: 400px;margin-top: 20px;">
-		<img alt="" src="/image/people-2.png"
+		<img alt="" src="/<?=$member->pic?>"
 			style="width: 214px; float: left; margin: 10px;">
 		<div style="margin: 10px 10px 40px; float: left; width: 546px;">
 			<p style="text-align: left; font-size: 30px; line-height: 40px;"><?=$member->name?></p>
 			<p
 				style="text-align: left; font-size: 20px; line-height: 30px; border-bottom: 1px solid #ccc;"><?=$member->position?></p>
 			<p style="text-align: left; font-size: 16px; line-height: 25px;"><?=$member->email?></p>
-			<p>$member->username</p>
+			<p><?=$member->summary?></p>
 		</div>
 	</div>
 	<?php endif;?>
