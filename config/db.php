b21551b58e3a318a47ece7f1277d10c7e134ed38
Tom Cao <jiepengthegreat@126.com>
2015-12-18 19:25:49 +0800

    typo

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=appmissevan1.mysql.rds.aliyuncs.com;dbname=app_missevan_soo',
-    'username' => 'missevan',
-    'password' => '1111111111',
+    'dsn' => 'mysql:host=localhost;dbname=zhcapital',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
