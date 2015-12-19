cb6e0f6562e1945290ee0ce8e213c89e7e36e904
DarkTan <yqjiang@gmail.com>
2015-12-19 20:59:12 +0800

    增加时间日期作者来源

--- views/site/article.php
+++ views/site/article.php
@@ -6,8 +6,16 @@
 		<?php echo $article[0]->title;?>
 	</div>
 	<?php if($article[0]->type != 4):?>
+	<div class="info">
 	<div class="time">
 	时间：<?php echo date("Y/m/d",$article[0]->date);?>
+	</div>
+	<div class="from">
+	文章来源：<?php echo $article[0]->from;?>
+	</div>
+	<div class="auth">
+	作者：<?php echo $article[0]->auth;?>
+	</div>
 	</div>
 	<?php endif;?>
 	<div class="content">
