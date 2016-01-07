5f0d861190323ba5d84733fe1f6b6721977a18d7
Tom <jiepengthegreat@126.com>
2016-01-07 21:34:55 +0800

    seo优化

--- views/site/people.php
+++ views/site/people.php
@@ -5,7 +5,7 @@
 	<?php foreach ($people as $key => $member):?>
 	<?php if ($key & 1) :?>
 	<div style="width: 100%; height: 400px; margin-top: 20px;">
-		<img alt="" src="/<?=$member->pic?>"
+		<img alt="<?=$member->name?>" src="/<?=$member->pic?>"
 			style="width: 214px; float: right; margin: 10px;">
 		<div style="margin: 10px 10px 40px; float: right; width: 546px;">
 			<p style="text-align: right; font-size: 30px; line-height: 40px;"><?=$member->name?></p>
@@ -17,7 +17,7 @@
 	</div>
 	<?php else :?>
 	<div style="width: 100%; height: 400px;margin-top: 20px;">
-		<img alt="" src="/<?=$member->pic?>"
+		<img alt="<?=$member->name?>" src="/<?=$member->pic?>"
 			style="width: 214px; float: left; margin: 10px;">
 		<div style="margin: 10px 10px 40px; float: left; width: 546px;">
 			<p style="text-align: left; font-size: 30px; line-height: 40px;"><?=$member->name?></p>
