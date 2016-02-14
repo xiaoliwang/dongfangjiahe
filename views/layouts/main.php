6f92aed6a9622f0dd4afa5d451f53415ffc0b11f
TomCao <jiepengthegreat@126.com>
2016-02-14 08:25:39 +0800

    typo

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -11,6 +11,7 @@ use app\assets\AppAsset;
 
 AppAsset::register($this);
 ?>
+
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
@@ -34,61 +35,62 @@ AppAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
-
 <div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => '<img style="width:40px;height:40px;margin-top:-10px;" src="/image/logo.png"/>',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-Left'],
-        'items' => [
-            ['label' => '主页', 'url' => ['/site/index']],
-            ['label' => '关于我们', 'url' => ['/site/about'],'items' => [
-['label' => '公司介绍', 'url' => '/site/about?type=0'],
-['label' => '股东介绍', 'url' => '/site/about?type=1'],
-['label' => '业务领域', 'url' => '/site/about?type=2'],
-['label' => '投资策略', 'url' => '/site/about?type=3'],
-]],
-			['label' => '公司团队', 'url' => ['/site/people']],
-			['label' => '投资案例', 'url' => ['/site/case']],
-			['label' => '新闻动态', 'url'=> ['/site/news'], 'items' => [
-				['label' => '公司动态', 'url' => '/site/news?type=1'],
-				['label' => '行业资讯', 'url' => '/site/news?type=2'],
-				['label' => '基金公告', 'url' => '/site/news?type=3'],
-    		]],
-        	['label' => '合作伙伴', 'url' => ['/site/partner']],
-            ['label' => '联系我们', 'url' => ['/site/contact']],
-        ],
-    ]);
-    echo Html::beginForm(['site/search'], 'get', ['class' => 'navbar-form navbar-right']);
-    echo Html::textInput('s', '',['class' => 'form-control', 'placeholder' => '搜索']);
-    echo '<span class="submit" style="margin-left:-30px;"><img src="/headerimg/search.png" style="width:20px;height:20px;"><button id="submit-btn" type="submit" style="display:none;"></span>';
-    echo Html::endForm();
+<?php
+NavBar::begin([
+    'brandLabel' => Html::image('/image/logo.png', ['style' => 'width:40px;height:40px;margin-top:-10px']),
+    'brandUrl' => Yii::$app->homeUrl,
+    'options' => [
+        'class' => 'navbar-inverse navbar-fixed-top',
+    ],
+]);
+echo Nav::widget([
+    'options' => ['class' => 'navbar-nav navbar-Left'],
+    'items' => [
+        ['label' => '主页', 'url' => ['/site/index']],
+        ['label' => '关于我们', 'url' => ['/site/about'],'items' => [
+            ['label' => '公司介绍', 'url' => '/site/about?type=0'],
+            ['label' => '股东介绍', 'url' => '/site/about?type=1'],
+            ['label' => '业务领域', 'url' => '/site/about?type=2'],
+            ['label' => '投资策略', 'url' => '/site/about?type=3'],
+        ]],
+		['label' => '公司团队', 'url' => ['/site/people']],
+		['label' => '投资案例', 'url' => ['/site/case']],
+		['label' => '新闻动态', 'url'=> ['/site/news'], 'items' => [
+			['label' => '公司动态', 'url' => '/site/news?type=1'],
+			['label' => '行业资讯', 'url' => '/site/news?type=2'],
+			['label' => '基金公告', 'url' => '/site/news?type=3'],
+        ]],
+    	['label' => '合作伙伴', 'url' => ['/site/partner']],
+        ['label' => '联系我们', 'url' => ['/site/contact']],
+    ],
+]);
+echo Html::beginForm(['site/search'], 'get', ['class' => 'navbar-form navbar-right']);
+echo Html::textInput('s', '',['class' => 'form-control', 'placeholder' => '搜索']);
+echo '<span class="submit" style="margin-left:-30px;"><img src="/headerimg/search.png" style="width:20px;height:20px;"><button id="submit-btn" type="submit" style="display:none;"></span>';
+echo Html::endForm();
     
-    NavBar::end();
-    ?>
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= $content ?>
+NavBar::end();
+?>
+<?= Breadcrumbs::widget([
+    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+]) ?>
+<?= $content ?>
 </div>
 
-	<footer class="footer">
-		<div class="container">
-			<p style="float:left;position:relative;line-height:25px;font-size:12px;" >
-			<?=Html::a('隐私政策', '/site/policy?type=0', ['style' => 'color:#9d9d9d'])?>&nbsp;<?=Html::a('使用条款', '/site/policy?type=1', ['style' => 'color:#9d9d9d'])?></p>
-			<p style="margin:auto;position:absolute;line-height:25px;font-size:12px;left:0;right:0;width:200px" >京ICP备15064520号-1</p>
-		</div>
-	</footer>
+<footer class="footer">
+	<div class="container">
+		<p style="float:left;position:relative;line-height:25px;font-size:12px;" >
+        	<?=Html::a('隐私政策', '/site/policy?type=0', ['style' => 'color:#9d9d9d'])?>&nbsp;
+            <?=Html::a('使用条款', '/site/policy?type=1', ['style' => 'color:#9d9d9d'])?>
+        </p>
+		<p style="margin:auto;position:absolute;line-height:25px;font-size:12px;left:0;right:0;width:200px" >京ICP备15064520号-1</p>
+	</div>
+</footer>
 <script type="text/javascript">
-        $('span.submit').click(function(){
-        	$('#submit-btn').click();
-        });
+    $('span.submit').click(function(){
+    	$('#submit-btn').click(); 
+    });
 </script>
 <?php $this->endBody() ?>
 </body>
