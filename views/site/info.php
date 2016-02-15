2bdc994610cb4c458cc6a24c6cdd7c74be99abdf
Tom <jiepengthegreat@126.com>
2016-02-15 09:55:05 +0800

    修改文字

--- views/site/info.php
+++ views/site/info.php
@@ -11,10 +11,10 @@
 		<p style="font-size: 30px; font-weight: 100; text-align: center">公司介绍</p>
 		<div style="margin-top: 20px;">
 			<img
-				style="width: 200px; margin-left: 10px; float: left; margin: 0 30px 0 130px;"
-				alt="公司简介" src="/image/about-2.png">
+				style="width: 370px; margin-left: 10px; float: left; margin: 0 30px 0 130px;"
+				alt="公司简介" src="/headerimg/all.jpg">
 			<p
-				style="font-size: 16px; font-weight: 100; width: 300px; margin-left: 10px; float: left; line-height: 30px;">北京正晖基金管理有限公司（简称正晖资本）是中国证券投资基金业协会备案登记的专注于股权投资及管理的专业机构，拥有一支经验丰富精诚合作的管理团队。公司光管分别来自与国内各大银行、基金公司或投资机构，平均金融从业经历逾十年均有丰富的投资管理和市场经验。</p>
+				style="font-size: 16px; font-weight: 100; width: 230px; margin-left: 10px; float: left; line-height: 30px;">北京正晖基金管理有限公司（简称正晖资本）是中国证券投资基金业协会备案登记的专注于股权投资及管理的专业机构，拥有一支经验丰富精诚合作的管理团队。公司光管分别来自与国内各大银行、基金公司或投资机构，平均金融从业经历逾十年均有丰富的投资管理和市场经验。</p>
 		</div>
 	</div>
 </div>
