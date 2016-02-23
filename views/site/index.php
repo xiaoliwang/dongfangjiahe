e0dcbeac2bac2a9cbfa17daf0858c9d8dd489a5f
Tom <jiepengthegreat@126.com>
2016-02-23 18:20:37 +0800

    首页修改

--- views/site/index.php
+++ views/site/index.php
@@ -1,57 +1,77 @@
-<div
-	style="position: relative; top: 50px; left: 0px; right: 0px; height: 300px;">
+<style>
+.head1 {
+	font-size: 20px;
+	padding-bottom: 12px;
+	font-family: SimSun;
+}
+.point {
+	width: 6px;
+	height: 6px;
+	background-color: #999;
+	position: relative;
+	float: left;
+	margin: 9px 10px 0 9px;
+}
+.small_point {
+	width: 4px;
+	height: 4px;
+	border-radius: 3px;
+	background-color: #999;
+	position: relative;
+	float: left;
+	margin: 9px 10px 0 9px;
+}
+</style>
+<span class="point"></span>
+<div style="position: relative; top: 50px; height: 300px;">
 	<div class="swiper-container swiper-container-horizontal">
 		<div class="swiper-wrapper"
 			style="transition-duration: 0ms; transform: translate3d(0px, 0px, 0px);">
 			<?php foreach ($frontpages as $frontpage):?>
-			<div class="swiper-slide" style="background-image: url(/<?=$frontpage->pic?>);">
-				<!-- 	<div class="ani" style="position: absolute;bottom:100px;background-color: rgba(0, 0, 0, 0.44);color: #dfdfdf;width: 38%;height: 150px;padding:0px;" swiper-animate-effect="fadeIn" swiper-animate-duration="2s" swiper-animate-delay="0s">
-				<p class="ani" style="color:#fff;font-size:40px;line-height:40px;font-weight:600;text-align:right;padding:10px 80px 5px;width:100%;border-bottom:1px solid #fff" swiper-animate-effect="fadeInLeft" swiper-animate-duration="2s" swiper-animate-delay="0.5s"><?=$frontpage->title?></p>
-				<p class="ani" style="color:#fff;font-size:14px;line-height:20px;font-weight:400;padding:10px 20px;width:100%" swiper-animate-effect="fadeInLeft" swiper-animate-duration="2s" swiper-animate-delay="1s"><?=$frontpage->summary?></p>
-			</div> -->
-			</div>
+			<div class="swiper-slide" style="background-image: url(/<?=$frontpage->pic?>);"></div>
 			<?php endforeach;?>
 		</div>
 	</div>
 	<div class="swiper-pagination">
 	</div>
 </div>
-<div class="page-contanier" style="margin-top: 80px; width: 900px">
-	<div style="margin: 0; left: 0; top: 0px; width: 600px;float: left;height: 40px;font-size: 20px;line-height: 40px;">
-	正晖资讯
-	</div>
-	<div style="margin: 0;width: 240px;float: left;line-height: 40px;height: 40px;font-size: 20px;margin-left: 60px;">案例分享</div>
-	<div style="margin: 0; left: 0; top: 0; width: 600px;">
-		<a href="/site/about?type=0">
-			<div class="news first">
-				<div class="img" style="width: 140px; height: 120px; float: left;">
-					<img alt="正晖资本:小而美" src="http://pic.58pic.com/58pic/14/76/61/15A58PICaQW_1024.jpg" style="width: 112px; height: 112px; margin:4px 14px;">
-				</div>
+<div class="page-contanier" style="margin-top: 80px; width:80%;max-width:1200px;min-width:900px;">
+	<div style="float:left; width:73%; height:220px;">
+		<div class="head1">
+			<div class="text" style="width: 100%;"><span class="point"></span>正晖资讯</div>
+		</div>
+		<img src="/headerimg/28.jpg" 
+				style="height:100px;width:100px;float:left;border:2px solid #fff; box-shadow:0 0 20px #bbb;"/>
+			<div style="position:relative;">
+			<div style="position:absolute;margin-left:114px;top:0px;">
 				<div class="title"
-					style="width: 400px; margin-left: 10px; font-size: 12px; float: left; line-height: 15px; color: #000; ">正晖资本:小而美</div>
+					style="width: 100%; margin-left: 10px; font-size: 12px; float: left; line-height: 15px; color: #000; ">正晖资本:小而美</div>
+				
 				<div class="intro"
-					style="width: 400px; float: left; margin-left: 10px;font-size: 12px; height: 90px; overflow: hidden; color: #999;line-height: 22px;margin-top:10px;">
-					这是一群经过了海外资本资本市场、国内主板中小板创业板多层次资本市场精炼的人。<br />
-					这是一群有情怀、有理想、资深的专业人士。<br />
-					这是一群环抱着共同目标与愿景的人。<br />
-					这是志趣相投、彼此认同的一家人。
+					style="width: 100%; float: left; margin-left: 10px;font-size: 12px; height: 90px; overflow: hidden; color: #999;line-height: 22px;margin-top:10px;">
+					&nbsp;&nbsp;&nbsp;&nbsp;这是一群经过了海外资本资本市场、国内主板中小板创业板多层次资本市场历练的人。这是一群有情怀、有理想、资深的专业人士。这是一群环抱着共同目标与愿景的人。这是志趣相投、彼此认同的一家人。
 				</div>
 			</div>
-		</a>
-		<div style="width: 600px;margin-top: 10px;float: left; margin-bottom: 80px;">
+		</div>
+		<div style="width: 100%;margin-top: 10px;float: left; margin-bottom: 80px;">
 		<?php foreach ($news as $new) :?>
-		<a href="/site/article?id=<?= $new->id ?>" style="display: block;color: #999;width: 280px;margin-right: 20px;font-size: 12px; float: left; line-height: 24px;"><?= $new->title?><span style="float: right"><?= date('Y-m-d', $new->date)?></span></a>
+		<a href="/site/article?id=<?= $new->id ?>" style="display: block;color: #999;width: 50%;padding-right: 20px;box-sizing:border-box;font-size: 12px; float: left; line-height: 24px;">
+			<span class="small_point"></span><?= $new->title?><span style="float: right"><?= date('Y-m-d', $new->date)?></span>
+		</a>
 		<?php endforeach;?>
 		</div>
 	</div>
-	<div class="swiper-container2" style="margin: 0;width: 260px; position: absolute; right: 0; top: 0;background-color: white;height: 180px;overflow:hidden;margin-top:40px;">
+	<div style="float:right; width:220px; height:220px;position:relative;">
+		<div class="head1"><div class="text" style="width: 200px;"><span class="point"></span>案例分析</div></div>
+
+		<div class="swiper-container2" style="margin: 0;width: 100%; position: absolute; right: 0; top: 0;background-color: white;height: 180px;overflow:hidden;margin-top:40px;">
 	   
 		<div class="swiper-wrapper" style="">
 		<?php //for($i=0;$i<4;$i++):?>
 		<div class="swiper-slide">
 		
 			<?php foreach($cases as $case) :?>
-				<a href="/site/case?year=2015" class="icon" style="margin-left: 30px;margin-bottom: 10px;float: left;display: block;width: 80px;height: 80px;">
+				<a href="/site/case?year=2015" class="icon" style="margin-right: 30px;margin-bottom: 10px;float: left;display: block;width: 80px;height: 80px;">
 					<img src="/<?=$case->pic?>" alt="" style="width: 80px;height: 80px;" >
 				</a>
 			<?php endforeach;?>
@@ -59,29 +79,16 @@
 		<?php //endfor;?>
 		</div>
 	</div>
+	</div>
 </div>
+
+
 <script>
-/**
-	function changeSlide() {
-		if()
-		$('.swiper-container .swiper-slide.swiper-slide-active').next().addClass('swiper-slide-active');
-		$('.swiper-container .swiper-slide.swiper-slide-visible').next().addClass('swiper-slide-visible');
-		$('.swiper-container .swiper-slide.swiper-slide-visible').removeClass('swiper-slide-visible');
-		$('.swiper-container .swiper-slide.swiper-slide-active').removeClass('swiper-slide-active');
-		
-	}**/
-    var swiper = new Swiper('.swiper-container', {
-    	'loop' : true,
-    	'speed':1000,
-    	'autoplay' : 3000,
-        'pagination': '.swiper-pagination',
-        'paginationClickable': true
-    });
-   // setTimeout('changeSlide()', 3000);
-    /*var swiper2 = new Swiper('.swiper-container2', {
-    	'loop' : true,
-    	'speed':1000,
-    	'autoplay' : 2000,
-        'mode' : 'vertical'
-    });*/
+var swiper = new Swiper('.swiper-container', {
+	'loop' : true,
+	'speed':1000,
+	'autoplay' : 3000,
+    'pagination': '.swiper-pagination',
+    'paginationClickable': true
+});
 </script>
---
+++ views/site/index_bac.php
@@ -0,0 +1,84 @@
+<div
+	style="position: relative; top: 50px; left: 0px; right: 0px; height: 300px;">
+	<div class="swiper-container swiper-container-horizontal">
+		<div class="swiper-wrapper"
+			style="transition-duration: 0ms; transform: translate3d(0px, 0px, 0px);">
+			<?php foreach ($frontpages as $frontpage):?>
+			<div class="swiper-slide" style="background-image: url(/<?=$frontpage->pic?>);">
+				<!-- 	<div class="ani" style="position: absolute;bottom:100px;background-color: rgba(0, 0, 0, 0.44);color: #dfdfdf;width: 38%;height: 150px;padding:0px;" swiper-animate-effect="fadeIn" swiper-animate-duration="2s" swiper-animate-delay="0s">
+				<p class="ani" style="color:#fff;font-size:40px;line-height:40px;font-weight:600;text-align:right;padding:10px 80px 5px;width:100%;border-bottom:1px solid #fff" swiper-animate-effect="fadeInLeft" swiper-animate-duration="2s" swiper-animate-delay="0.5s"><?=$frontpage->title?></p>
+				<p class="ani" style="color:#fff;font-size:14px;line-height:20px;font-weight:400;padding:10px 20px;width:100%" swiper-animate-effect="fadeInLeft" swiper-animate-duration="2s" swiper-animate-delay="1s"><?=$frontpage->summary?></p>
+			</div> -->
+			</div>
+			<?php endforeach;?>
+		</div>
+	</div>
+	<div class="swiper-pagination">
+	</div>
+</div>
+<div class="page-contanier" style="margin-top: 80px; width: 1200px">
+	<div style="margin: 0; left: 0; top: 0px; width: 900px;float: left;height: 40px;font-size: 20px;line-height: 40px;">
+	正晖资讯
+	</div>
+	<div style="margin: 0;width: 240px;float: left;line-height: 40px;height: 40px;font-size: 20px;margin-left: 60px;">案例分享</div>
+	<div style="margin: 0; left: 0; top: 0; width: 900px;">
+		<a href="/site/about?type=0">
+			<div class="news first">
+				<div class="img" style="width: 140px; height: 120px; float: left;">
+					<img alt="正晖资本:小而美" src="http://i6.download.fd.pchome.net/t_960x600/g1/M00/04/1D/oYYBAFHjo5iIUZ5rAAWT6vomydwAAAvlQGYgMYABZQC186.jpg" style="width: 112px; height: 112px; margin:4px 14px;">
+				</div>
+				<div class="title"
+					style="width: 700px; margin-left: 10px; font-size: 12px; float: left; line-height: 15px; color: #000; ">正晖资本:小而美</div>
+				<div class="intro"
+					style="width: 700px; float: left; margin-left: 10px;font-size: 12px; height: 90px; overflow: hidden; color: #999;line-height: 22px;margin-top:10px;">
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;这是一群经过了海外资本资本市场、国内主板中小板创业板多层次资本市场历练的人。这是一群有情怀、有理想、资深的专业人士。这是一群环抱着共同目标与愿景的人。这是志趣相投、彼此认同的一家人。
+				</div>
+			</div>
+		</a>
+		<div style="width: 600px;margin-top: 10px;float: left; margin-bottom: 80px;">
+		<?php foreach ($news as $new) :?>
+		<a href="/site/article?id=<?= $new->id ?>" style="display: block;color: #999;width: 280px;margin-right: 20px;font-size: 12px; float: left; line-height: 24px;"><?= $new->title?><span style="float: right"><?= date('Y-m-d', $new->date)?></span></a>
+		<?php endforeach;?>
+		</div>
+	</div>
+	<div class="swiper-container2" style="margin: 0;width: 260px; position: absolute; right: 0; top: 0;background-color: white;height: 180px;overflow:hidden;margin-top:40px;">
+	   
+		<div class="swiper-wrapper" style="">
+		<?php //for($i=0;$i<4;$i++):?>
+		<div class="swiper-slide">
+		
+			<?php foreach($cases as $case) :?>
+				<a href="/site/case?year=2015" class="icon" style="margin-left: 30px;margin-bottom: 10px;float: left;display: block;width: 80px;height: 80px;">
+					<img src="/<?=$case->pic?>" alt="" style="width: 80px;height: 80px;" >
+				</a>
+			<?php endforeach;?>
+		</div>
+		<?php //endfor;?>
+		</div>
+	</div>
+</div>
+<script>
+/**
+	function changeSlide() {
+		if()
+		$('.swiper-container .swiper-slide.swiper-slide-active').next().addClass('swiper-slide-active');
+		$('.swiper-container .swiper-slide.swiper-slide-visible').next().addClass('swiper-slide-visible');
+		$('.swiper-container .swiper-slide.swiper-slide-visible').removeClass('swiper-slide-visible');
+		$('.swiper-container .swiper-slide.swiper-slide-active').removeClass('swiper-slide-active');
+		
+	}**/
+    var swiper = new Swiper('.swiper-container', {
+    	'loop' : true,
+    	'speed':1000,
+    	'autoplay' : 3000,
+        'pagination': '.swiper-pagination',
+        'paginationClickable': true
+    });
+   // setTimeout('changeSlide()', 3000);
+    /*var swiper2 = new Swiper('.swiper-container2', {
+    	'loop' : true,
+    	'speed':1000,
+    	'autoplay' : 2000,
+        'mode' : 'vertical'
+    });*/
+</script>
