1054e92a85708f7395b5b089c1203e9548f73d24
Tom <jiepengthegreat@126.com>
2017-02-28 11:51:11 +0800

    修改联系和投资方

--- views/site/contact.php
+++ views/site/contact.php
@@ -2,20 +2,20 @@
 	style="background-image: url(/headerimg/1.jpg);"></div>
 <div class="page-contanier">
 	<img alt="" style="width: 640px; margin: 100px 80px 0;"
-		src="/image/map-1.jpg" />
+		src="/image/map.jpg" />
 
 	<div class="row sqs-row" style="padding: 15px 95px; width: 830px; height: 200px;">
 			<div class="sqs-block html-block sqs-block-html">
 				<div class="sqs-block-content" style="position:absolute;width:100%;">
 	<img src="/image/qrcode.jpg" style="position:absolute;width:160px;height:160px;right:160px;top:0px;"></img>
 	<p style="">
-		北京市 朝阳区霄云路26号鹏润大厦A1907室
+		北京市 朝阳区将台路2号诺金写字楼1608室
 	</p>
 			<p>
 			邮箱：	<a href="mailto:zxd@GemsrayCapital.com">zxd@GemsrayCapital.com</a>
 			</p>
 			<p>
-			电话：+86 10 5108 7734
+			电话：+86 10 6431 2799
 			</p>
 			<p>
 			传真：+86 10 5108 7732
--- views/site/invest.php
+++ views/site/invest.php
@@ -14,8 +14,18 @@
 	<div class="about-2"
 		style="position: relative; width: 800px; left: 200px; top: 100px; height: 500px;">
 		<div style="margin-top: 0px;">
-			<p
-				style="font-size: 30px; font-weight: 100; text-align: center;">股东介绍</p>
+            <p style="font-size: 30px; font-weight: 100; text-align: center;">股东介绍</p>
+            <!--<div style="margin: 20px 40px 0 100px; width: 100%">
+                <img style="width: 260px; margin-left: 10px; float: left; margin: 0 0px 20px; height: 174px;" alt="新恒基集团" src="/image/about-3-2.png">
+                <div style="float: left; width: 540px">
+                    <p style="font-size: 20px; font-weight: 300; text-align: center;">四川大东</p>
+                    <p style="float: left; font-size: 16px; line-height: 30px; font-weight: 300; text-align: center; margin: 20px 10px; height: 174px;">
+                        四川大东集团经历二十多年的发展，采取全资、控股、参股、关联等方式对下属企业进行管理。集团业务广泛涵盖地产开发、城市建设，精细化工及新能源、环保等产业并逐步向资本市场、股权投资转型升级。
+                    </p>
+                </div>
+
+            </div>-->
+
 			<!--<div style="margin: 20px 40px 0 100px; width: 260px; float: left;">
 				<img
 					style="width: 260px; margin-left: 10px; float: left; margin: 0 0px 20px;"
@@ -27,14 +37,14 @@
 					新恒基集团经历二十多年的发展，创造了年增长30%的发展速度，目前集团总资产已超过200亿元，旗下拥有17家控股公司，多家上市公司。形成了以地产开发为核心，涵盖写字楼及酒店管理、金融投资、网络通信、高新科技、生物制药、 多元化综合型企业集团。
 				</p>
 			</div>-->
-			<div style="margin: 20px 40px; width: 260px; float: left;">
+			<div style="margin: 20px 200px; width: 400px; float: left;">
 				<img
-					style="width: 260px; margin-left: 10px; float: left; margin: 0 0px 20px; height: 174px;"
+					style="width: 400px; margin-left: 10px; float: left; margin: 0 0px 20px;"
 					alt="新恒基集团" src="/image/about-3-2.png">
 				<p
-					style="font-size: 20px; font-weight: 300; text-align: center; margin-top: 40px; width: 260px;">四川大东</p>
+					style="font-size: 20px; font-weight: 300; text-align: center; margin-top: 40px; width: 400px;">四川大东</p>
 				<p
-					style="font-size: 16px; line-height: 30px; font-weight: 300; text-align: center; margin-top: 10px; width: 260px; height: 174px;">
+					style="font-size: 16px; line-height: 30px; font-weight: 300; text-align: center; margin-top: 10px; width: 400px; height: 174px;">
 					四川大东集团经历二十多年的发展，采取全资、控股、参股、关联等方式对下属企业进行管理。集团业务广泛涵盖地产开发、城市建设，精细化工及新能源、环保等产业并逐步向资本市场、股权投资转型升级。
 				</p>
 			</div>
